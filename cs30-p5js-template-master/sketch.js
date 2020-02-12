bb8f67305319d9d66a2970866ef79c62edb29cac
bonjo112 <45461880+bonjo112@users.noreply.github.com>
2020-02-12 14:35:06 -0600

    files

---
+++ cs30-p5js-template-master/sketch.js
@@ -0,0 +1,25 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+let mario;
+
+function preload() {
+  mario = loadImage("assets/);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill("red");
+  // rect(mouseX, mouseY, 75, 75);
+  image(fish, mouseX, mouseY);
+}
