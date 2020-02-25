c303425de8295acefac8cc9c77dc1d86f25df3fa
bonjo112 <45461880+bonjo112@users.noreply.github.com>
2020-02-25 14:36:55 -0600

    started

--- Interactive Scene/template/sketch.js
+++ Interactive Scene/template/sketch.js
@@ -1,5 +1,5 @@
-// Project Title
-// Your Name
+// Interactive Scene
+// Nathan Androsoff
 // Date
 //
 // Extra for Experts:
@@ -7,9 +7,40 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400 , 400);
 }
 
 function draw() {
+  displayCircleGrid();
   background(220);
+}
+
+function displayCircleGrid() {
+  //1
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50);
+  //2 
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 75, 75);
+  //3
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50);
+  //4
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50);
+  //5
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50);
+  //6
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50); 
+  //7
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50);
+  //8
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50);
+  //9 
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50); 
 }
