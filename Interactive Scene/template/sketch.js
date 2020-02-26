01b7b4a8db919445358957587bdd4b2f9c5153d5
bonjo112 <45461880+bonjo112@users.noreply.github.com>
2020-02-26 14:16:18 -0600

    first day

--- Interactive Scene/template/sketch.js
+++ Interactive Scene/template/sketch.js
@@ -4,43 +4,71 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let Tut;
+let a;
+let b;
+let c;
+let d;
+let e;
+let possibleSolutions;
 
 function setup() {
-  createCanvas(400 , 400);
-}
-
-function draw() {
-  displayCircleGrid();
+  createCanvas(windowHeight , windowHeight);
   background(220);
 }
 
+
+function draw() {
+  definingSolutions();
+  displayCircleGrid();
+}
+
+function definingSolutions() {
+  let Tut = [1,1,1,1,1,1,1,1,1];
+  let a = [1,1,0,1,2,1,1,1,1];
+  let b = [1,2,1,2,1,1,1,2,1];
+  let c = [3,1,1,1,2,1,1,1,1];
+  let d = [2,3,1,0,3,1,2,2,0];
+  let e = [2,0,1,2,4,3,1,1,0];
+  let possibleSolutions = [Tut,a,b,c,d,e];
+}
+
 function displayCircleGrid() {
+  textAlign(CENTER, CENTER);
+  textSize(32);
   //1
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50);
+  ellipse(width / 2 , height / 2 , 50, 50);
+  fill("black");
+  text("1", width/2, height/2);
   //2 
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 75, 75);
+  ellipse(width / 2 + 100 , height / 2  , 50,50 );
+  fill("black");
+  text("2", width/2 + 100 , height/2);
   //3
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50);
+  ellipse(width / 2 + 200 , height / 2  , 50, 50);
   //4
   fill("red");
   ellipse(width / 2, height / 2 - 65, 50, 50);
+  fill("black");
+  text("4", width/2, height/2 - 65);
   //5
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50);
+  ellipse(width / 2 + 100, height / 2 - 65, 50, 50);
   //6
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50); 
+  ellipse(width / 2 + 200, height / 2 - 65, 50, 50); 
   //7
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50);
+  ellipse(width / 2, height / 2 - 130, 50, 50);
+  fill("black");
+  text("7", width/2, height/2 - 130);
   //8
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50);
+  ellipse(width / 2 + 100, height / 2 - 130, 50, 50);
   //9 
   fill("red");
-  ellipse(width / 2, height / 2 - 65, 50, 50); 
+  ellipse(width / 2 + 200, height / 2 - 130, 50, 50); 
 }
